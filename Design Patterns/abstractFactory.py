7a4ef95ae515422427a02ebd5e5130eb651b79e0
Aneetta Sara <39122867+aneettasara@users.noreply.github.com>
2021-04-29 09:56:38 +0530

    Add files via upload

---
+++ Design Patterns/abstractFactory.py
@@ -0,0 +1,39 @@
+# Abstract and Concrete Factory
+# Abstract and Concrete Products
+
+class Dog:
+    def speak(self):
+        return "bark!"
+
+    def __str__(self):
+        return "Dog"
+
+class DogFactory:       # Concrete Factory
+    def get_pet(self):
+        return Dog()
+
+    def get_food(self):
+        return "Dog Food"
+        
+class PetStore:         # Abstract Factory
+    def __init__(self, pet_factory=None):
+        self.pet_factory = pet_factory
+
+    def show_pet(self):
+
+        pet = self.pet_factory.get_pet() 
+        pet_food = self.pet_factory.get_food() 
+
+        print(f"Our pet is '{pet}'!")
+        print(f"Our pet says hello by '{pet.speak()}'")
+        print(f"Its food is '{pet_food}'!")
+
+# Main Function
+
+# Concrete Factory
+factory = DogFactory()
+
+# Abstract Factory with the instance of Concrete Factory
+shop = PetStore(factory)
+
+shop.show_pet()
